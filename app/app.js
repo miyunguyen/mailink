c190d583a245d32a6cb744f675b9f862158dfd51
miyunguyen <ya15510@gmail.com>
2026-08-20 21:08:26 +0700

    sort import

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import 'dotenv/config';
 import bodyParser from 'body-parser';
+import 'dotenv/config';
+import express from 'express';
 
 import { globalResponseHeaders } from './middlewares/cors.middleware.js';
 import { errorHandler } from './middlewares/error.middleware.js';
